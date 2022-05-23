c5cd80f4f822cab34b6813d320aa69d48815688b
Prasiinus <valentind9@hotmail.fr>
2022-05-23 10:37:57 +0200

    ajout utilisateur avec annotations

--- src/main/java/com/inti/SpringBootValidationThymeleaf_/controller/MainController.java
+++ src/main/java/com/inti/SpringBootValidationThymeleaf_/controller/MainController.java
@@ -1,11 +1,13 @@
 package com.inti.SpringBootValidationThymeleaf_.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MainController 
 {
 
-	@GetMapping("hello")
+	@GetMapping("/hello")
 	public String hello()
 	{
 		return "hello";
